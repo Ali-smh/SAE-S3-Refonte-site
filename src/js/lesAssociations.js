0659aaba924a116477bec8403f89e66e2cfbde90
huanling <huanling1236@gmail.com>
2024-11-12 15:39:22 +0100

    Update associations

---
+++ src/js/lesAssociations.js
@@ -0,0 +1,153 @@
+export const lesAssociations = [
+    {
+        id: 1,
+        nom: "Alcool Ecoute VIENNE",
+        adresse: "2 Rue du Pré des Rosées, 86000 Poitiers",
+        numero:"05 49 47 04 76",
+        mail :"alcoolecoute.js86@free.fr",
+        president:"Présidente : Monique HERIGAULT",
+        lien: "https://www.google.fr/maps/place/Joie+et+Sant%C3%A9/@46.5717964,0.3669099,15z/data=!4m6!3m5!1s0x47fdbe9507f6bdb1:0x602d84f92bed0271!8m2!3d46.5759228!4d0.3741841!16s%2Fg%2F1vd94g56?entry=ttu&g_ep=EgoyMDI0MTAxNS4wIKXMDSoASAFQAw%3D%3D",
+        img :'../img/map/map1_Vienne.png',
+        region :"Nouvelle-Aquitaine"
+    },
+    {
+        id: 2,
+        nom: "Alcool Ecoute MARNE",
+        adresse: "5 Rue de la Charrière, 51000 Châlons-en-Champagne",
+        numero :"06 30 24 54 48",
+        mail :"alcool.ecoute.joie.sante.marne@gmail.com",
+        president:"Président : Philippe RENAULT",
+        lien: "https://www.google.fr/maps/place/5+Rue+de+la+Charri%C3%A8re,+51000+Ch%C3%A2lons-en-Champagne/@48.9568224,4.3732609,17.1z/data=!4m6!3m5!1s0x47ebe09de7578a09:0x8711210820a13c1c!8m2!3d48.9569847!4d4.3731957!16s%2Fg%2F11csjm5qrj?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img :'../img/map/map2_Marne.png',
+        region :"Grand Est"
+    },
+    {
+        id: 3,
+        nom: "Alcool Ecoute AIN",
+        adresse: "103 Av. Félix Mangini, 01110 Plateau d'Hauteville",
+        numero: "04 74 35 10 15",
+        mail :"pierre.bourquin1@aliceadsl.fr",
+        president:"Président : Pierre BOURQUIN",
+        lien:"https://www.google.fr/maps/place/103+Av.+F%C3%A9lix+Mangini,+01110+Plateau+d'Hauteville/@45.9786055,5.6054546,15z/data=!4m6!3m5!1s0x478b5df59d0e6dcd:0x8124e8606b5bd3d6!8m2!3d45.9787976!4d5.6061495!16s%2Fg%2F11c5k91mhx?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img :'../img/map/map3_Ain.png',
+        region :"Auvergne-Rhône-Alpes"
+    },
+    {
+        id: 4,
+        nom: "Alcool Ecoute HAUTE-SAVOIE",
+        adresse: "8 Bd du Bennevy 156, 74500 Évian-les-Bains",
+        numero: "06 26 60 08 27",
+        mail :"alcoolecoutefin@yahoo.fr",
+        president:"Président : Jean-Gilles VINCENT",
+        lien:"https://www.google.fr/maps/place/8+Bd+du+Bennevy+156,+74500+%C3%89vian-les-Bains/@46.3972802,6.5883595,17z/data=!3m1!4b1!4m5!3m4!1s0x478c3dfcc8bb10a9:0x9870045db4d8adb3!8m2!3d46.3972802!4d6.5883595?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img :'../img/map/map4_HauteSavoie.png',
+        region :"Auvergne-Rhône-Alpes"
+    },
+    {
+        id: 5,
+        nom: "Alcool Ecoute CHER",
+        adresse: "33 Imp. Camille Pelletan, 18000 Bourges",
+        numero:"06 79 36 29 77",
+        president:"Présidente : LALANNE Patricia",
+        lien:"https://www.google.fr/maps/place/alcool+assistance+du+cher/@47.0816457,2.3779974,15z/data=!3m1!5s0x47fa95d0a4a6a903:0x46e5ce97d87bcdcc!4m10!1m2!2m1!1sAlcool+Ecoute+CHER+18000+Bourges!3m6!1s0x47fa95d48d92d1bd:0xd69705a45483a05!8m2!3d47.0816457!4d2.3867521!15sCiBBbGNvb2wgRWNvdXRlIENIRVIgMTgwMDAgQm91cmdlc5IBHGFsY29ob2xpc21fdHJlYXRtZW50X3Byb2dyYW3gAQA!16s%2Fg%2F11lgdstbwt?entry=ttu&g_ep=EgoyMDI0MTAxMy4wIKXMDSoASAFQAw%3D%3D",
+        img :'../img/map/map5_Cher.png',
+        region :"Centre-Val de Loire"
+    },
+    {
+        id: 6,
+        nom: "Alcool Ecoute VAL de MARNE",
+        adresse: "5 Rue Carnot, 94600 Choisy-le-Roi",
+        numero: "06 09 80 45 08",
+        mail :"alcoolecoute94@gmail.com",
+        president:"Président : Bernard COLIN",
+        lien:"https://www.google.fr/maps/place/Centre+de+Soins+d'Accompagnement+et+de+Pr%C3%A9vention+en+Addictologie+Henri+Duch%C3%AAne+(CSAPA)/@48.7664626,2.4071601,17z/data=!3m1!4b1!4m6!3m5!1s0x47e6748679228971:0xa0723a2238a9fb37!8m2!3d48.7664591!4d2.409735!16s%2Fg%2F11b6dq9w1l?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img :'../img/map/map6_ValMarne.png',
+        region :"Île-de-France"
+    },
+    {
+        id: 7,
+        nom: "Alcool Ecoute SARTHE",
+        adresse: "66 bis Rue de Belfort, 72000 Le Mans",
+        numero: "02 43 14 15 40",
+        mail :"aejs72@orange.fr",
+        president:"Président : Didier PETITHOMME",
+        lien:"https://www.google.fr/maps/place/4+Rue+d'Arcole/@47.9999748,0.1920567,17z/data=!4m6!3m5!1s0x47e28f34a78b146f:0x80d669d359b7bf1f!8m2!3d48.0000566!4d0.1919243!16s%2Fg%2F11b8v40wsr?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img: '../img/map/map7_Sarthe.png',
+        region :"Pays de la Loire"
+    },
+    {
+        id: 8,
+        nom: "Alcool Ecoute CALVADOS",
+        adresse: "Rue Eugène Moulin, 14270 Mézidon-Canon",
+        numero :"02 31 20 00 31",
+        president:"Responsable : Noël OLLIVIER",
+        lien:"https://www.google.fr/maps/place/Rue+Eug%C3%A8ne+Moulin,+14270+M%C3%A9zidon+Vall%C3%A9e+d'Auge/@49.0747001,-0.0832328,17.13z/data=!4m6!3m5!1s0x480a7483abf66e1d:0x23a12376dac60e2c!8m2!3d49.0748359!4d-0.0832495!16s%2Fg%2F1tdgjndt?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img: '../img/map/map8_Calvados.png',
+        region :"Nouvelle-Aquitaine"
+    },
+    {
+        id: 9,
+        nom: "Alcool Ecoute VOSGES" ,
+        adresse: "29 Rue de la République, 88130 Charmes",
+        numero: "03 29 38 09 88",
+        mail :"alcoolecoute-88@wanadoo.fr",
+        president:"Président : François SIMONIN",
+        lien:"https://www.google.fr/maps/place/29+Rue+de+la+R%C3%A9publique,+88130+Charmes/@48.3703007,6.2928825,17z/data=!3m1!4b1!4m6!3m5!1s0x4793701e5ecb67e7:0x61632afbe78285bc!8m2!3d48.3703007!4d6.2928825!16s%2Fg%2F11c1fllx58?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img: '../img/map/map9_Vosges.png',
+        region :"Normandie"
+    },
+    {
+        id: 10,
+        nom: "Alcool Ecoute HAUTE-MARNE" ,
+        adresse: "60 Pl. Aristide Briand, 52000 Chaumont",
+        numero: "03 29 08 55 16",
+        mail :"noel.mielle@orange.fr",
+        president:"Présidente : MIELLE Martine",
+        lien:"https://www.google.fr/maps/place/Association+des+Maires+de+la+Haute+Marne/@48.1174857,5.1400917,17z/data=!3m1!4b1!4m6!3m5!1s0x47ece57b04d23679:0xbac2ab75ddb3355b!8m2!3d48.1174821!4d5.1426666!16s%2Fg%2F11g_qpr9w9?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img: '../img/map/map10_HauteMarne.png',
+        region :"Grand Est"
+    },
+    {
+        id: 11,
+        nom: "Alcool Ecoute ALLIER",
+        adresse: "18 Rue Roger Loury, 03100 Montluçon",
+        numero: "06 07 57 91 00",
+        mail :"alcool-ecoute-c.journal@wanadoo.fr",
+        president:"Président : Evelyne ROCARD",
+        lien:"https://www.google.fr/maps/place/18+Rue+Roger+Loury,+03100+Montlu%C3%A7on/@46.3620463,2.5901533,20z/data=!4m6!3m5!1s0x47f0a78580ad8951:0x260bb918d21652a4!8m2!3d46.3620541!4d2.5902182!16s%2Fg%2F11gfhqg2f_?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img: '../img/map/map11_Allier.png',
+        region :"Auvergne-Rhône-Alpes"
+    },
+    {
+        id: 12,
+        nom: "Alcool Ecoute SOMME",
+        adresse: "Pl. du Général Leclerc, 80450 Camon",
+        numero: "06 86 96 89 87",
+        mail :"bourgeois.jacky@orange.fr",
+        president:"Président : Jacky BOURGEOIS",
+        lien:"https://www.google.fr/maps/place/Mairie+de+Camon/@49.8878815,2.3415685,17z/data=!4m10!1m2!2m1!1sMairie+Place+du+G%C3%A9n%C3%A9ral+Leclerc+80450+CAMON!3m6!1s0x47e785917263d07f:0x22fb78f557e6f6b!8m2!3d49.8878815!4d2.3437572!15sCi1NYWlyaWUgUGxhY2UgZHUgR8OpbsOpcmFsIExlY2xlcmMgODA0NTAgQ0FNT06SAQljaXR5X2hhbGzgAQA!16s%2Fg%2F11b6dsk601?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img: '../img/map/map12_Somme.png',
+        region :"Hauts-de-France"
+    },
+    {
+        id: 13,
+        nom: "Alcool Ecoute LOIRET",
+        adresse: "62 Rue Marceau, 45120 Châlette-sur-Loing",
+        numero: "02 38 89 32 20",
+        mail :"venonclaudius@orange.fr",
+        president:"Président : Jean Yves BATISSE",
+        lien:"https://www.google.fr/maps/place/62+Rue+Marceau,+45120+Ch%C3%A2lette-sur-Loing/@48.0105227,2.7302901,17z/data=!3m1!4b1!4m6!3m5!1s0x47e57fd3707a621f:0x921b7887291216f2!8m2!3d48.0105227!4d2.7302901!16s%2Fg%2F11c5j39zgh?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img: '../img/map/map13_Loiret.png',
+        region :"Centre-Val de Loire"
+    },
+    {
+        id: 14,
+        nom: "Alcool Ecoute AISNE",
+        adresse: "18 Rue Victor Hugo, 02700 Tergnier",
+        numero: "03 23 57 51 12",
+        president: "Président : Anne-Marie TROCQ",
+        lien:"https://www.google.fr/maps/place/18+Rue+Victor+Hugo,+02700+Tergnier/@49.6556802,3.2906079,17z/data=!3m1!4b1!4m6!3m5!1s0x47e83fede64199e7:0x638a465e27d34941!8m2!3d49.6556768!4d3.2931828!16s%2Fg%2F11c5n80tpf?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D",
+        img: '../img/map/map14_Aisne.png',
+        region :"Hauts-de-France"
+    }
+];
